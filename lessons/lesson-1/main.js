84ad782c97aa599d81c27700e7073a4a6005407c
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-02-03 07:18:03 +0200

    add lesson-1 js

--- lessons/lesson-1/main.js
+++ lessons/lesson-1/main.js
@@ -1,4 +1,29 @@
-console.log(22);
-console.log('hello');
+// console.log(22);
+// console.log('hello');
+//
+// document.write('hello');
 
-document.write('hello');
+let str = 'Hello';//string 'hello' "hello" `hello`
+console.log(str);
+
+const PI = 3.14;
+console.log(PI);
+
+str = 'Hello everybody';
+console.log(str);
+
+{
+    let a = 10;
+    var b = 11;
+    console.log(a);
+    console.log(b);
+}
+// console.log(a);
+// console.log(b);
+
+
+let strText = 'Hello';
+console.log(typeof strText);
+
+let numText = 34;
+console.log(typeof numText);
