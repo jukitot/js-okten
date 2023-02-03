e86916498234b24b0c05f12f6027f56bb8f16beb
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-02-03 11:49:29 +0200

    add homework-1(done) js

---
+++ lessons/lesson-1/homework-1/main.js
@@ -0,0 +1,56 @@
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let bool1 = true;
+let bool2 = false;
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(bool1);
+console.log(bool2);
+
+
+let firstName = 'Anastasiia';
+let middleName = 'Romanovna';
+let lastName = 'Irzhytska';
+let fullName = firstName + ' ' + middleName + ' ' +  lastName;
+console.log(fullName);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(a);
+console.log(typeof b);
+console.log(b);
+console.log(typeof c);
+console.log(c);
+
+let name = prompt('What is your name?', firstName);
+alert(`Your name is ${name} `);
+console.log(name);
+
+let lastname = prompt('What is your surname?', lastName);
+alert(`Your surname is ${lastname}`);
+console.log(lastname);
+
+let year = prompt('What is your year of born?', 2003);
+alert(`Your year of born is ${year} `);
+console.log(year);
