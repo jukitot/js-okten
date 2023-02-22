56652c839720c5d4e95d4270eb3caac69b400447
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-02-22 21:13:41 +0200

    add prev less-8

--- lessons/lesson-8/prev/main.js
+++ lessons/lesson-8/prev/main.js
@@ -14,16 +14,29 @@
 // }
 // new User('jkk', 23, true, {name: 'ksjk'});
 
-function User(name, age, status, wifeName, wifeAge){
+// function User(name, age, status, wifeName, wifeAge){
+//     this.name = name;
+//     this.age = age;
+//     this.status = status;
+//     this.wife = {name: wifeName, age: wifeAge}
+//     this.greeting = function () {
+//         console.log('hi');
+//     }
+// }
+//
+// let user = new User('mkk', 122, true, 'kjkj', 22);
+// console.log(user);
+// user.greeting();
+//prototype call apply bind
+function User(name, age){
     this.name = name;
     this.age = age;
-    this.status = status;
-    this.wife = {name: wifeName, age: wifeAge}
-    this.greeting = function () {
-        console.log('hi');
-    }
 }
 
-let user = new User('mkk', 122, true, 'kjkj', 22);
-console.log(user);
+console.log(User);
+User.prototype.greeting = function () {
+    return `hello my name is ${this.name}`;
+};
 
+let user = new User('vasya', 123);
+console.log(user.greeting());
