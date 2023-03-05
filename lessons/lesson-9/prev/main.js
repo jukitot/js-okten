4d3ee8c9c0ac96209ad974a65ebd1470a618a319
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-03-05 13:17:34 +0200

    add prev less-9

--- lessons/lesson-9/prev/main.js
+++ lessons/lesson-9/prev/main.js
@@ -40,3 +40,33 @@ for (const nodeListOfElement of nodeListOf) {
 }
 
 //manipulation DOM elements
+let target = document.getElementById('target');
+console.log(target.innerText);
+// target.innerText = 'okten';
+target.style.background = 'silver';
+// console.log(target.classList);
+target.classList.add('foo');
+target.classList.add('bar');
+// console.log(target.classList.keys());
+// console.log(target.classList.item(0));
+// console.log(target.classList.contains('asd'));
+// target.classList.toggle('bar');
+
+console.log(target.getAttribute('xxx'));
+console.log(target.getAttribute('id'));
+console.log(target.getAttribute('class'));
+console.log(target.getAttribute('style'));
+
+console.log(target.previousElementSibling);
+console.log(target.nextElementSibling);
+console.log(target.children);
+console.log(target.childNodes);
+
+target.innerHTML = '<b>aksjdkj</b>'
+console.log(target.outerHTML);
+
+let collectionOfDiv = document.getElementsByClassName('point');
+for (const collectionOfDivElement of collectionOfDiv) {
+    collectionOfDivElement.innerText = 'skfjdskjfj';
+    collectionOfDivElement.classList.add('dlkflk')
+}
